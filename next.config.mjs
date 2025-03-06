7ebb594f0e50d3bd8cf5c1d179919f00c4b43d46
MoatezLita <moatezlitaiem@gmail.com>
2025-03-06 15:48:15 +0100

    config: seo sitemap and robots config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-  }
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['yalors.tn'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
 export default nextConfig
